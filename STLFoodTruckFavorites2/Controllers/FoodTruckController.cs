eebc064bcf3f47e5f4b8ae6d131546d6c21ed908
ccartagena879 <ccartagena879@gmail.com>
2019-08-01 14:43:30 -0500

    FoodTruck Create working

---
+++ STLFoodTruckFavorites2/Controllers/FoodTruckController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using STLFoodTruckFavorites2.Data;
+using STLFoodTruckFavorites2.ViewModels;
+
+namespace STLFoodTruckFavorites2.Controllers
+{
+    public class FoodTruckController : Controller
+    {
+        private ApplicationDbContext context;
+
+        public FoodTruckController(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            FoodTruckCreateViewModel model = new FoodTruckCreateViewModel();
+            return View(model);
+        }
+
+        [HttpPost]
+        public IActionResult Create(FoodTruckCreateViewModel model)
+        {
+            model.Persist(context);
+            return View(model);
+        }
+    }
+}
---
+++ STLFoodTruckFavorites2/ViewModels/FoodTruckCreateViewModel.cs
@@ -0,0 +1,31 @@
+﻿using STLFoodTruckFavorites2.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace STLFoodTruckFavorites2.ViewModels
+{
+    public class FoodTruckCreateViewModel
+    {
+        private ApplicationDbContext context;
+
+        public FoodTruckCreateViewModel() { }
+
+        public void Persist(ApplicationDbContext context)
+        {
+            Models.FoodTruck foodTruck = new Models.FoodTruck()
+            {
+                Name = this.Name,
+                Description = this.Description,
+            };
+            context.FoodTrucks.Add(foodTruck);
+            context.SaveChanges();
+        }
+
+
+        //public int ID { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
