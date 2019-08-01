556ce07e612db6c39138b14f09f744c6e197f432
ccartagena879 <ccartagena879@gmail.com>
2019-08-01 13:09:10 -0500

    models and initial migration complete

--- STLFoodTruckFavorites2/Data/ApplicationDbContext.cs
+++ STLFoodTruckFavorites2/Data/ApplicationDbContext.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using System.Text;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using STLFoodTruckFavorites2.Models;
 
 namespace STLFoodTruckFavorites2.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
+        public DbSet<FoodTruck> FoodTrucks { get; set; }
+        public DbSet<Location> Locations { get; set; }
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
--- STLFoodTruckFavorites2/Data/Migrations/20190801180323_AddFoodTruckAndLocationClasses.Designer.cs
+++ STLFoodTruckFavorites2/Data/Migrations/20190801180323_AddFoodTruckAndLocationClasses.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using STLFoodTruckFavorites2.Data;
 
 namespace STLFoodTruckFavorites2.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20190801180323_AddFoodTruckAndLocationClasses")]
+    partial class AddFoodTruckAndLocationClasses
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
